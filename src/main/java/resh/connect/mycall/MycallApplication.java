6ab1332f40449c071740539292c18c65d4e09bb8
ahremenkoartem <ahremenkoartem@mail.ru>
2025-09-21 00:33:59 +0300

    Initial commit

---
+++ src/main/java/resh/connect/mycall/MycallApplication.java
@@ -0,0 +1,13 @@
+package resh.connect.mycall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/resh/connect/mycall/MycallApplicationTests.java
@@ -0,0 +1,13 @@
+package resh.connect.mycall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
